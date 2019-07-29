243cc08ed67d2611c86d5c530ea4dc6f94244b49
Christian Cannata <christian1488@hotmail.it>
2019-07-29 18:05:55 +0200

    minor fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './lib'
-import { Stripe, baseStyle } from './stripeElements'
+import {
+  Stripe,
+  baseStyle
+} from './stripeElements'
 import StripeElement from './StripeElement'
 import Card from './Card'
 import CardExpiry from './CardExpiry'
@@ -7,7 +10,7 @@ import CardCvc from './CardCvc'
 import CardNumber from './CardNumber'
 import PostalCode from './PostalCode'
 
-module.exports = {
+export default {
   Card,
   CardNumber,
   CardExpiry,
@@ -15,15 +18,37 @@ module.exports = {
   PostalCode,
   StripeElement,
   baseStyle,
-  get instance() { return Stripe.instance },
-  get createToken() { return Stripe.createToken },
-  get createSource() { return Stripe.createSource },
-  get retrieveSource() { return Stripe.retrieveSource },
-  get paymentRequest() { return Stripe.paymentRequest },
-  get redirectToCheckout() { return Stripe.redirectToCheckout },
-  get retrievePaymentIntent() { return Stripe.retrievePaymentIntent },
-  get handleCardPayment() { return Stripe.handleCardPayment },
-  get handleCardSetup() { return Stripe.handleCardSetup },
-  get confirmPaymentIntent() { return Stripe.confirmPaymentIntent },
-  get createPaymentMethod() { return Stripe.createPaymentMethod }
+  get instance() {
+    return Stripe.instance
+  },
+  get createToken() {
+    return Stripe.createToken
+  },
+  get createSource() {
+    return Stripe.createSource
+  },
+  get retrieveSource() {
+    return Stripe.retrieveSource
+  },
+  get paymentRequest() {
+    return Stripe.paymentRequest
+  },
+  get redirectToCheckout() {
+    return Stripe.redirectToCheckout
+  },
+  get retrievePaymentIntent() {
+    return Stripe.retrievePaymentIntent
+  },
+  get handleCardPayment() {
+    return Stripe.handleCardPayment
+  },
+  get handleCardSetup() {
+    return Stripe.handleCardSetup
+  },
+  get confirmPaymentIntent() {
+    return Stripe.confirmPaymentIntent
+  },
+  get createPaymentMethod() {
+    return Stripe.createPaymentMethod
+  }
 }
